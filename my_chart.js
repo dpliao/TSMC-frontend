d40bb6de74ea9e424072861664b46faffcef2a3c
dpliao <dunpeiliao@gmail.com>
2022-05-30 22:52:26 +0800

    html and js

---
+++ my_chart.js
@@ -0,0 +1,54 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+// const labels = Utils.months({count: 7});
+// const data = {
+//   labels: labels,
+//   datasets: [{
+//     label: 'My First Dataset',
+//     data: [65, 59, 80, 81, 56, 55, 40],
+//     fill: false,
+//     borderColor: 'rgb(75, 192, 192)',
+//     tension: 0.1
+//   }]
+// };
+
+var n = [];
+var arr = [];
+for(i=1;i<=365*2;i++){
+    n.push(i);
+    arr.push(Math.floor(Math.random()*(100-50+1))+50);
+};
+
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: n,
+        datasets: [{
+            label: '# of search',
+            data: arr,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
